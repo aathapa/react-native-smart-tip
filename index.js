75cf47aac78f9e24252b6e6d2585a55094269035
Aayush Thapa <aayushthapa7@gmail.com>
2020-07-16 12:54:17 +0545

    fix module

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-export * from './lib'
+import WToast from './toast/Toast'
+import WSnackBar from './snackbar/SnackBar'
+import WModal from './modaltoast/ModalToast'
+import WModalShowToastView from './modaltoast/ModalShowToastView'
+
+export {WModal,WSnackBar,WToast,WModalShowToastView}
